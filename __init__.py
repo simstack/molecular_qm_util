389edf05470a39342e1118a0ee8e1999b3ecc1bc
Wolfgang Wenzel <wenzel.int@gmail.com>
2026-08-20 14:02:50 +0200

    Stop importing removed pymatgen_scripts package.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,9 @@
 from .obabel_scripts.compute_smiles import compute_smiles
 from .obabel_scripts.similes_to_molecule import smiles_to_molecule
 from .obabel_scripts.compute_iupac_name import compute_iupac_name
-from .pymatgen_scripts.molecule_to_pymatgen import molecule_to_pymatgen, pymatgen_to_molecule
 
 __all__ = [
     "compute_smiles",
     "smiles_to_molecule",
     "compute_iupac_name",
-    "molecule_to_pymatgen",
-    "pymatgen_to_molecule",
 ]
